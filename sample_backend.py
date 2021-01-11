769d77ee54912bca2c672a02bfd37d8a53356586
mgray07 <mac95618@gmail.com>
2021-01-11 15:05:15 -0800

    First

---
+++ sample_backend.py
@@ -0,0 +1,40 @@
+from flask import Flask
+app = Flask(__name__)
+
+users = { 
+   'users_list' :
+   [
+      { 
+         'id' : 'xyz789',
+         'name' : 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id' : 'abc123', 
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'ppp222', 
+         'name': 'Mac',
+         'job': 'Professor',
+      }, 
+      {
+         'id' : 'yat999', 
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id' : 'zap555', 
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+
+@app.route('/users')
+def get_users():
+   return users
+
+
+
