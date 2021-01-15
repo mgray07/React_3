570ee6fd2a4454c6b450f6b7bb9a2ffdf00ea0a0
mgray07 <mac95618@gmail.com>
2021-01-14 22:17:32 -0800

    fri

--- sample_backend.py
+++ sample_backend.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+import random, string
 
 app = Flask(__name__)
 CORS(app)
@@ -67,9 +68,12 @@ def get_users():
       return users
    elif request.method == 'POST':
       userToAdd = request.get_json()
+      #if userToAdd['id'] == '':
+      newId = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6))
+      userToAdd['id'] = newId
       users['users_list'].append(userToAdd)
       resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
+      resp.status_code = 201 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
    elif request.method == 'DELETE':
